ffae5a64d8035d644db6ec320d5257cf36b237d0
Semyon Stulnikov <92747308+SemyonSt@users.noreply.github.com>
2024-09-16 14:22:55 +0300

    del

---
+++ netology/src/page/Header3.jsx
@@ -0,0 +1,57 @@
+// import './Header.css';
+
+// function Header () {
+//   return (
+//     // <div className="App">
+//       <header className="App-header">
+//         <div className="lol">
+//           <div className='header_button'>
+//             <img className="logo" src="https://static.tildacdn.com/tild3065-3833-4163-a636-643830306133/F1ULL_logo_color_bl.svg" alt="logo" />
+//             <div className='header_button_menu'>
+//               <div className='text'>Меню</div>
+//             </div>
+//           </div>
+//           <img className="girl" src="https://optim.tildacdn.com/tild6234-3836-4665-b465-626633363930/-/format/webp/_1.png" alt="Girl" />
+//           {/* <svg xmlns="http://www.w3.org/2000/svg" width="1161" height="614" viewBox="0 0 1161 614" fill="none">
+//             <path fillRule="evenodd" clipRule="evenodd" d="M50 85C22.3858 85 0 107.386 0 135V564C0 591.614 22.3858 614 50 614H348.015C365.556 614 380 582.041 380 564.5C380 531.639 406.639 505 439.5 505H720.5C753.361 505 780 531.639 780 564.5C780 582.041 794.444 614 811.985 614H1111C1138.61 614 1161 591.614 1161 564V135C1161 107.386 1138.61 85 1111 85H985C957.386 85 935 62.1142 935 34.5C935 20.0483 925.078 0 910.626 0H247.374C232.922 0 223 20.0483 223 34.5C223 62.1142 200.614 85 173 85H50Z" fill="url(#paint0_radial_15_1134)" />
+//             <defs>
+//               <radialGradient id="paint0_radial_15_1134" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(580.5 307) rotate(90.0608) scale(471.5 891.55)">
+//                 <stop stopColor="#55E2AF" />
+//                 <stop offset="1" stopColor="#002A6A" />
+//               </radialGradient>
+//             </defs>
+//           </svg> */}
+//           <div className='header_info'>
+//             <div className='main_header'>
+//               Как использовать нейросети
+//             </div>
+//             <div className='main_subheader'>
+//               в графическом дизайне
+//             </div>
+//           </div>
+
+//           <div className='header_guide' >
+//             <div className='btn_guide'>Бесплатный гайд </div>
+//             <div className='info_guide'>Получите бесплатный гайд
+//               по искусственному интеллекту, который поможет улучшить работу графического дизайнера </div>
+//           </div>
+
+//           <div className='atom_star'>
+//             <img className="atom_star" src="https://optim.tildacdn.com/tild3963-3265-4233-b237-383232393132/-/resize/210x/-/format/webp/n_konovalov_3D_rende.png" alt="atom" />
+
+//           </div>
+//           <div className='atom_lightning'>
+//             <img className="atom_lightning" src="https://optim.tildacdn.com/tild3331-3961-4034-a430-623938353534/-/resize/332x/-/format/webp/n_konovalov_3D_rende.png" alt="atom" />
+//           </div>
+
+//           <div className='download_btn'>
+//             Получить гайд
+//           </div>
+
+//         </div>
+//       </header>
+//     // </div>
+//   );
+// }
+
+// export default Header;
